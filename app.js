b813d5529cfc8a862b2c7f672e9d761a54457722
‘abswhite1git config --global user.email ‘abigailwhite213@gmail.com <abigailwhite213@gmail.comgit config --global user.name abswhite1>
2017-01-06 15:47:21 -0800

    add array functionality working

--- app.js
+++ app.js
@@ -61,7 +61,6 @@ function sumAndMultiply(a,b,c){ //eslint-disable-line
   var sumResult2 = sum(sumResult[0], c);
   console.log(sumResult2[0]);
   console.log(sumResult2[1]);
-  console.log(sumResult2[2]);
 
   var multiplyResult = multiply(a,b);
   console.log(multiplyResult[0]);
@@ -98,12 +97,20 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
-
+  var sumResult = sum(testArray[0], testArray[1]);
+  console.log(sumResult[0]);
+  console.log(sumResult[1]);
+  var sumResult2 = sum(sumResult[0], testArray[2]);
+  console.log(sumResult2[0]);
+  console.log(sumResult2[1]);
+  var message = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + sumResult2[0] + ' is their sum.';
+  console.log(message);
+  return [sumResult2[0], message];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
